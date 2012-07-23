5267f039617a64ad7410213e998ad613a9421262
Ivan Stroganov <knowhere@gmail.com>
2012-07-23 11:12:44 -0700

    new app with its rvm gemset

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hnclown::Application.config.secret_token = '51bbb497e30d89c1f15758489ce80d5d3ad5f033daf54e63ee2d3b9b02dee16d567a496e59e95b974ae2ce27030a212c22e644d2a0a98b521872acb74766f932'
