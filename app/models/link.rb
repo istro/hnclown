11bfa5a83786b8e098419657852ca192d386ac5f
Brian Parks + Ivan Stroganov <apprentice+bp+is@devbootcamp.com>
2012-07-23 21:46:37 -0700

    standardized url input

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,14 +1,24 @@
-class Link < ActiveRecord::Base
+class Link  < ActiveRecord::Base
   belongs_to :users
   attr_accessible :rank, :title, :url
 
-  before_validation :prepend_http
+  before_validation :prepend_www_http
   validates :url, :uniqueness => true
 
 
 
 
-  def prepend_http
-    self.url = "http://#{self.url}" unless self.url =~ /^http:\/\//
+  def prepend_www_http
+    puts 'starting..'
+    if self.url.starts_with? 'http://www'
+      self.url
+    elsif self.url.starts_with? 'http://'
+      self.url = "http://www.#{self.url[7..-1]}"
+    elsif self.url.starts_with? 'www.'
+      self.url = "http://www.#{self.url[4..-1]}"
+    else
+      self.url = "http://www.#{self.url}"
+    end
+    self.url
   end
 end
