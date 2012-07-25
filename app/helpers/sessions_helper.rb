3eb3a938f4b8c9f9adc131133616a94927214688
Brian Parks + Ivan Stroganov <apprentice+bp+is@devbootcamp.com>
2012-07-24 19:38:08 -0700

    user auth DONE!

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,7 +20,6 @@ module SessionsHelper
 
   def sign_out
     self.current_user = nil
-    # cookies.delete(:remember_token)
-    cookies[:remember_token] = nil
+    cookies.delete(:remember_token)
   end
 end
