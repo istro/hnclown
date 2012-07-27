9e09a58fd2603fc1a54474a95accfd16c29495d0
Brian Parks + Ivan Stroganov <apprentice+bp+is@devbootcamp.com>
2012-07-26 17:30:44 -0700

    add functionality to allow users to edit their links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   def index
-    @links = Link.all
+    @links = Link.order('created_at DESC')
   end
 
   def new
@@ -8,15 +8,30 @@ class LinksController < ApplicationController
   end
 
   def create
-    warn 'here are me params before creating the link'
-    warn params.inspect
     Link.create(params[:link])
     redirect_to "/links"
   end
 
   def show
-    @link = Link.find_by_url(params[:url])
-    redirect_to "http://#{@link.url}"
+    @link = Link.find_by_id(params[:id])
+    redirect_to links_path
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.user_id != current_user.id || @link.created_at + 900 < Time.now
+      redirect_to links_path
+    else
+      warn 'here are params'
+      warn params[:link]
+      @link.update_attributes(params[:link])
+      flash.now[:notice] = "Link was updated."
+      render 'edit'
+    end
   end
 
 end
